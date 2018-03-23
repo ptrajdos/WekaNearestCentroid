15928f01fc9c4f8c1a15e9c77716669da096bbc7
PT <pawel.trajdos@pwr.edu.pl>
2018-03-23 09:17:05 +0100

    1. Initial setup.

---
+++ src/main/java/weka/classifiers/functions/NearestCentroidClassifier.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package weka.classifiers.functions;
+
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instances;
+
+/**
+ * @author pawel
+ *
+ */
+public class NearestCentroidClassifier extends AbstractClassifier {
+
+	/**
+	 * 
+	 */
+	public NearestCentroidClassifier() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.Classifier#buildClassifier(weka.core.Instances)
+	 */
+	@Override
+	public void buildClassifier(Instances data) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
