c7053a3ed93da60bb5fb535d168f39047417ecbe
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2023-07-18 22:14:53 +0200

    1. AbstractPrototypeTest -- Additional tests.

--- src/test/java/weka/classifiers/functions/nearestCentroid/prototypes/AbstractPrototypeTest.java
+++ src/test/java/weka/classifiers/functions/nearestCentroid/prototypes/AbstractPrototypeTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 import junit.framework.TestCase;
+import weka.classifiers.Classifier;
 import weka.classifiers.functions.nearestCentroid.IClusterPrototype;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -14,6 +15,10 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.tools.SerialCopier;
+import weka.tools.data.RandomDataGenerator;
+import weka.tools.data.RandomDoubleGenerator;
+import weka.tools.data.RandomDoubleGeneratorGaussian;
+import weka.tools.tests.DistributionChecker;
 
 /**
  * @author pawel trajdos
@@ -70,6 +75,40 @@ public abstract class AbstractPrototypeTest extends TestCase {
 		return dataset;
 	}
 	
+	public void checkPrototype(IClusterPrototype proto, Instances dataset) {
+		assertTrue("Is cnetroid valid", proto.isValid());
+		Instance centroid = proto.getCenterPoint();
+		
+		double distance = proto.distance(dataset.get(0), dataset.get(0));
+		assertTrue("The same instances distance", Utils.eq(distance, 0.0));
+		
+		distance = proto.distance(dataset.get(0), dataset.get(1));
+		assertFalse("Distance not Nan", Double.isNaN(distance));
+		assertTrue("Distance finite", Double.isFinite(distance));
+		assertTrue("Different instances distance", Utils.grOrEq(distance, 0.0));
+	
+		
+		distance = proto.distance(centroid);
+		assertFalse("Distance to centroid from centroid not Nan (general check)", Double.isNaN(distance));
+		assertTrue("Distance to centroid from centroid finite (general check)", Double.isFinite(distance));
+		assertTrue("Distance to Centroid (from centroid) ", Utils.eq(distance, 0.0));
+		
+		distance =  proto.distance(dataset.get(0));
+		assertFalse("Distance not Nan (from centroid)", Double.isNaN(distance));
+		assertTrue("Distance finite (from centroid)", Double.isFinite(distance));
+		assertTrue("Distance to Centroid (from different instance) ", Utils.grOrEq(distance, 0.0));
+		
+		for(Instance instance: dataset) {
+			
+			distance =  proto.distance(instance);
+			
+			assertFalse("Distance not Nan (general check)", Double.isNaN(distance));
+			assertTrue("Distance finite (general check)", Double.isFinite(distance));
+			assertTrue("Distance to Centroid (general check) ", Utils.grOrEq(distance, 0.0));
+			
+		}
+		
+	}
 	
 	public void testPrototype() {
 		IClusterPrototype proto = this.getPrototype();
@@ -79,28 +118,88 @@ public abstract class AbstractPrototypeTest extends TestCase {
 		try {
 	    	assertFalse("Is cnetroid invalid", proto.isValid());
 			proto.build(dataset);
-			assertTrue("Is cnetroid valid", proto.isValid());
+			
+			this.checkPrototype(proto, dataset);
 			Instance centroid = proto.getCenterPoint();
-			
-			double distance = proto.distance(dataset.get(0), dataset.get(0));
-			assertTrue("The same instances distance", Utils.eq(distance, 0.0));
-			
-			distance = proto.distance(dataset.get(0), dataset.get(1));
-			assertTrue("Different instances distance", Utils.gr(distance, 0.0));
-		
 			assertTrue("Centroid check ", Utils.eq(centroid.toDoubleArray()[0], 0));
 			
-			distance = proto.distance(centroid);
-			assertTrue("Distance to Centroid (from centroid) ", Utils.eq(distance, 0.0));
-			
-			distance =  proto.distance(dataset.get(0));
-			assertTrue("Distance to Centroid (from different instance) ", Utils.gr(distance, 0.0));
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("An exception has been caught");
 		}
 		
 	}
+	
+	 public void testOnCondensedData() {
+		 IClusterPrototype proto = this.getPrototype();
+		 
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(0);
+		 gen.setNumStringAttributes(0);
+		 gen.setNumDateAttributes(0);
+		 RandomDoubleGenerator doubleGen = new RandomDoubleGeneratorGaussian();
+		 doubleGen.setDivisor(100000.0);
+		 gen.setDoubleGen(doubleGen );
+		 
+		 Instances dataset = gen.generateData();
+		 try {
+			 
+			 proto.build(dataset);
+			 this.checkPrototype(proto, dataset);
+						
+		} catch (Exception e) {
+			fail("An exception has been caught " + e.getMessage());
+		}
+	 }
+	 	 
+	 public void testOnNominalConvertedData() {
+		 IClusterPrototype proto = this.getPrototype();
+		 
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(20);
+		 gen.setNumStringAttributes(0);
+		 gen.setNumDateAttributes(0);
+		 gen.setNumNumericAttributes(0);
+		 gen.setNumClasses(2);
+		 gen.setMaxNumNominalValues(5);
+		 gen.setNumObjects(100);
+		 gen.setAllowUnary(true);
+		 
+		 Instances dataset = gen.generateData();
+		 try {
+			 
+			 proto.build(dataset);
+			 this.checkPrototype(proto, dataset);
+						
+		} catch (Exception e) {
+			fail("An exception has been caught " + e.getMessage());
+		}
+	 }
+	 
+	 
+	 public void testOnUnaryData() {
+		 IClusterPrototype proto = this.getPrototype();
+		 
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(10);
+		 gen.setNumStringAttributes(0);
+		 gen.setNumDateAttributes(0);
+		 gen.setNumNumericAttributes(0);
+		 gen.setNumClasses(2);
+		 gen.setMaxNumNominalValues(1);
+		 gen.setNumObjects(50);
+		 gen.setAllowUnary(true);
+		 
+		 Instances dataset = gen.generateData();
+		 try {
+			 
+			 proto.build(dataset);
+			 this.checkPrototype(proto, dataset);
+						
+		} catch (Exception e) {
+			fail("An exception has been caught " + e.getMessage());
+		}
+	 }
 	
 	public void testSerialization() {
 		try {
